36cac6672ecaccba879d04184a8e3c26279b75ad
Andrey Petrash <andrey@ekreative.com>
2016-01-18 16:49:10 +0200

    fix loginType Form

--- src/AppBundle/Form/LoginType.php
+++ src/AppBundle/Form/LoginType.php
@@ -70,62 +70,60 @@ class LoginType extends AbstractType
         $builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) use ($builder) {
             $form = $event->getForm();
             $data = $event->getData();
-            if ($data) {
-                $form
-                    ->add('loginType', 'choice', [
-                        'choices' => $this->getAvailableTypes((string)$builder->getForm()->getPropertyPath()),
-                        'expanded' => true,
-                        'attr' => [
-                            'class' => 'btn-group',
-                            'data-toggle' => 'buttons',
-                            'data-login-type-toggle' => null
-                        ],
-                        'data' => $data && $data->getLoginType() ? $data->getLoginType() : Login::TYPE_NONE,
-                    ])
-                    ->add('databaseName', null, [
-                        'attr' => [
-                            'data-login-type' => Login::TYPE_DB
-                        ]
-                    ])
-                    ->add('username', null, [
-                        'attr' => [
-                            'data-login-type' => implode(' ', [Login::TYPE_SITE, Login::TYPE_SSH, Login::TYPE_DB])
-                        ]
-                    ])
-                    ->add('password', null, [
-                        'attr' => [
-                            'data-login-type' => implode(' ', [Login::TYPE_SITE, Login::TYPE_SSH, Login::TYPE_DB])
-                        ]
-                    ])
-                    ->add('sshKey', null, [
-                        'attr' => [
-                            'help-block' => 'The text of a PEM file used for login',
-                            'data-login-type' => Login::TYPE_SSH
-                        ]
-                    ])
-                    ->add('hostname', null, [
-                        'attr' => [
-                            'help-block' => 'DNS name for this login',
-                            'data-login-type' => implode(' ', [Login::TYPE_SSH, Login::TYPE_DB])
-                        ]
-                    ])
-                    ->add('port', null, [
-                        'attr' => [
-                            'data-login-type' => implode(' ', [Login::TYPE_SSH, Login::TYPE_DB])
-                        ]
-                    ])
-                    ->add('url', null, [
-                        'attr' => [
-                            'data-login-type' => Login::TYPE_SITE
-                        ]
-                    ]);
-                if ($this->includeProxy) {
-                    $builder->add('proxyHost', new LoginType(false), [
-                        'attr' => [
-                            'data-login-type' => Login::TYPE_DB
-                        ]
-                    ]);
-                }
+            $form
+                ->add('loginType', 'choice', [
+                    'choices' => $this->getAvailableTypes((string)$builder->getForm()->getPropertyPath()),
+                    'expanded' => true,
+                    'attr' => [
+                        'class' => 'btn-group',
+                        'data-toggle' => 'buttons',
+                        'data-login-type-toggle' => null
+                    ],
+                    'data' => $data && $data->getLoginType() ? $data->getLoginType() : Login::TYPE_NONE,
+                ])
+                ->add('databaseName', null, [
+                    'attr' => [
+                        'data-login-type' => Login::TYPE_DB
+                    ]
+                ])
+                ->add('username', null, [
+                    'attr' => [
+                        'data-login-type' => implode(' ', [Login::TYPE_SITE, Login::TYPE_SSH, Login::TYPE_DB])
+                    ]
+                ])
+                ->add('password', null, [
+                    'attr' => [
+                        'data-login-type' => implode(' ', [Login::TYPE_SITE, Login::TYPE_SSH, Login::TYPE_DB])
+                    ]
+                ])
+                ->add('sshKey', null, [
+                    'attr' => [
+                        'help-block' => 'The text of a PEM file used for login',
+                        'data-login-type' => Login::TYPE_SSH
+                    ]
+                ])
+                ->add('hostname', null, [
+                    'attr' => [
+                        'help-block' => 'DNS name for this login',
+                        'data-login-type' => implode(' ', [Login::TYPE_SSH, Login::TYPE_DB])
+                    ]
+                ])
+                ->add('port', null, [
+                    'attr' => [
+                        'data-login-type' => implode(' ', [Login::TYPE_SSH, Login::TYPE_DB])
+                    ]
+                ])
+                ->add('url', null, [
+                    'attr' => [
+                        'data-login-type' => Login::TYPE_SITE
+                    ]
+                ]);
+            if ($this->includeProxy) {
+                $builder->add('proxyHost', new LoginType(false), [
+                    'attr' => [
+                        'data-login-type' => Login::TYPE_DB
+                    ]
+                ]);
             }
         });
     }
