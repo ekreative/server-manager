320152ab99a5882afc58c0f529c5fdf0b4cd0d69
Bulava Eduard <bulavaeduard@gmail.com>
2017-07-17 17:11:41 +0300

    update dir structure and symfony/symfony component

--- web/app_dev.php
+++ web/app_dev.php
@@ -8,10 +8,8 @@ use Symfony\Component\HttpFoundation\IpUtils;
 // read http://symfony.com/doc/current/book/installation.html#configuration-and-setup for more information
 //umask(0000);
 
-$loader = require_once __DIR__.'/../app/bootstrap.php.cache';
+$loader = require_once __DIR__.'/../app/autoload.php';
 Debug::enable();
-
-require_once __DIR__.'/../app/AppKernel.php';
 
 $kernel = new AppKernel('dev', true);
 $kernel->loadClassCache();
