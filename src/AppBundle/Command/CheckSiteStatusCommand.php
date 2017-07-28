0feb30e6a5e42a7a17708a335c3ce0f06748ec1b
smagolexandr <smagolexandr@gmail.com>
2017-07-28 18:01:15 +0300

    bug fixing

---
+++ src/AppBundle/Command/CheckSiteStatusCommand.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace AppBundle\Command;
+
+use AppBundle\Entity\Site;
+use Doctrine\Bundle\DoctrineBundle\Registry;
+use Doctrine\ORM\EntityManager;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class CheckSiteStatusCommand extends Command
+{
+    /**
+     * @var EntityManager
+     */
+    private $em;
+
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    public function __construct(Registry $doctrine, LoggerInterface $logger)
+    {
+        parent::__construct();
+
+        $this->em = $doctrine->getManager();
+        $this->logger = $logger;
+    }
+
+    public function configure()
+    {
+        $this
+            ->setName('app:check-site-status')
+            ->setDescription('Checks the supported sites for expiration of SLA.')
+        ;
+    }
+
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $sites = $this->em->getRepository(Site::class)->findBy(['status' => "Supported"]);
+        if (!$sites){
+            $this->logger->error('No sites to update.');
+            return;
+        }
+
+        $i = 0;
+        foreach($sites as $site) {
+            if ($site->getSlaEndAt() && $site->getSlaEndAt()  < new \DateTime()){
+                $i++;
+                $site->setStatus('UnSupported');
+            }
+        }
+
+        $this->em->flush();
+        $this->logger->info($i . "sites updated");
+    }
+}
--- src/AppBundle/Entity/Framework.php
+++ src/AppBundle/Entity/Framework.php
@@ -18,6 +18,12 @@ use Symfony\Component\Validator\Constraints as Assert;
  */
 class Framework implements AuthorEditorable
 {
+    const SYMFONY = 'symfony';
+    const JOOMLA = 'joomla';
+    const WORDPRESS = 'wordpress';
+    const DRUPAL_7 = 'drupal7';
+    const DRUPAL_8 = 'drupal8';
+
     use AuthorEditorableEntity;
     use TimestampableEntity;
 
@@ -54,6 +60,13 @@ class Framework implements AuthorEditorable
      * @ORM\OneToMany(targetEntity="Site", mappedBy="framework")
      */
     private $sites;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="key")
+     */
+    private $key;
 
     public function __construct()
     {
@@ -136,5 +149,23 @@ class Framework implements AuthorEditorable
     public function __toString()
     {
         return $this->name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getKey()
+    {
+        return $this->key;
+    }
+    /**
+     * @param string $key
+     *
+     * @return $this
+     */
+    public function setKey($key)
+    {
+        $this->key = $key;
+        return $this;
     }
 }
